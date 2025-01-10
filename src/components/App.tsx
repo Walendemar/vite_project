58476ea5c844646a9b3fcc0627ad0c1a661d3ca2
Walendemar <windowtothespace@gmail.com>
2025-01-10 15:01:15 +0300

    Init Vite project

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import { Fragment, useState } from 'react';
+import './App.css';
+
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Fragment key="4338231e-9fc7-49aa-9daf-d011e627645c">
+      <div>
+        Test
+      </div>
+    </Fragment>
+  )
+};
+
+export default App;
